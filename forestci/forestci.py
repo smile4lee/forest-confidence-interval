0220db5ec74669a80b613ed0b3096e5f9193e00e
lihaijun <lihaijun0629@gmail.com>
2020-12-29 16:35:26 +0800

    uses joblib Parallel loop

--- forestci/forestci.py
+++ forestci/forestci.py
@@ -16,6 +16,8 @@ from sklearn.ensemble._bagging import BaseBagging
 from .calibration import calibrateEB
 from .due import _due, _BibTeX
 
+from joblib import effective_n_jobs, Parallel, delayed
+
 __all__ = ("calc_inbag", "random_forest_error", "_bias_correction",
            "_core_computation")
 
@@ -273,7 +275,16 @@ def random_forest_error(
     if inbag is None:
         inbag = calc_inbag(X_train.shape[0], forest)
 
-    pred = np.array([tree.predict(X_test) for tree in forest]).T
+    # pred = np.array([tree.predict(X_test) for tree in forest]).T
+
+    def _predict_by_tree(_tree, _X):
+        return _tree.predict(_X)
+    # Parallel loop, returns values as a list
+    res = Parallel(n_jobs=forest.n_jobs, verbose=forest.verbose, prefer='threads')(
+        delayed(_predict_by_tree)(tree, X_test)
+        for tree in forest)
+
+    pred = np.array(res).T
 
     # the final predictions in forest
     # pred_mean_t = np.mean(pred, axis=1)
